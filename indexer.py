e0b584c3cbe701256a5144130236a8d091bc6a00
Dave Jain <jain.udbhav@gmail.com>
2021-02-10 13:26:53 +1100

    Add comments

--- indexer.py
+++ indexer.py
@@ -7,13 +7,15 @@ from org.apache.lucene.analysis.en import EnglishAnalyzer
 INDEXDIR = 'index'
 FILESDIR = 'wiki'
 
+# Initialise virtual machine
 lucene.initVM()
 
-analyzer = CJKAnalyzer()
+# Use English analyzer; Works best for this task
 analyzer = EnglishAnalyzer()
 
 indexer = engine.Indexer(INDEXDIR, analyzer = analyzer)
 
+# Index file name, content, title of each article and title text
 indexer.set('file', engine.Field.Text, stored = True)
 indexer.set('text', engine.Field.Text, stored = True)
 indexer.set('title', engine.Field.Text, stored = True)
@@ -31,6 +33,7 @@ for filename in os.listdir(FILESDIR):
 	for line in lines:
 		title,text = line.split(' ', 1)
 		if title not in prev:
+			# if title is different compared to previous line, save previous
 			prev_title = list(prev)[0]
 			tixt = prev_title.replace('_', ' ')
 			indexer.add(text = content, file = filename, title = prev_title, titleTxt = tixt)
@@ -38,6 +41,7 @@ for filename in os.listdir(FILESDIR):
 			prev.add(title)
 			content = text
 		else:
+			# otherwise append line to content
 			content = content + text
 
 	indexer.add(text = content, file = filename, title = title)
--- src.py
+++ src.py
@@ -24,6 +24,7 @@ tokenizer = AutoTokenizer.from_pretrained("roberta-large-mnli")
 model = AutoModelForSequenceClassification.from_pretrained("roberta-large-mnli")
 entailment = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer)
 
+# Extract named entities from query
 doc = nlp(src)
 names = ' '.join([ent.text for ent in doc.ents])
 
@@ -32,6 +33,7 @@ indexer = engine.Indexer(INDEXDIR, analyzer = analyzer)
 
 qry = QueryParser('titleTxt', analyzer).parse(names)
 
+# Search for named entities in title texts
 hits = indexer.search(qry, field = 'titleTxt', count = 3)
 
 mx = hits.maxscore
@@ -45,6 +47,8 @@ for hit in hits:
 			l_num, l_txt = line.split(' ', 1)
 			resl = entailment(l_txt + src)[0]
 			
+			# Add to results if the line contradicts or supports the query
+			# with high confidence
 			if resl['label'] in chklabs and resl['score'] > 0.95:
 				pkg = (hit['title'], hit['file'], resl['label'], resl['score'], line)
 				evidc_ls.append(pkg)
@@ -52,6 +56,7 @@ for hit in hits:
 
 
 if len(evidc_ls) == 0:
+	# If no evidence was found, search for whole sentence in article bodies
 	qry = QueryParser('text', analyzer).parse(src)
 
 	hits = indexer.search(qry, field = 'text', count = 3)
@@ -74,6 +79,7 @@ if len(evidc_ls) == 0:
 if len(evidc_ls) == 0:
 	print('Not enough evidence.')
 else:
+	# Print results
 	for idx, evidc in enumerate(evidc_ls):
 		print('---------------------------------------------------')
 		print('Artile: ',evidc[0], ' ; File: ', evidc[1], ' ; Verdict: ', evidc[2], ' ', evidc[3])
